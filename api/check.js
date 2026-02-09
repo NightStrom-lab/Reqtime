52a70be29e5a1de1ef1dc85b2d20657093979e28
NightStrom-lab <alexwijaya5554@gmail.com>
2026-02-09 22:48:05 +0700

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,12 +1,10 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res){
   const url = req.query.url;
   if(!url) return res.status(400).json({status:400, message:"URL required"});
 
   try{
     const controller = new AbortController();
-    const timeout = setTimeout(() => controller.abort(), 15000); // 15 detik
+    const timeout = setTimeout(() => controller.abort(), 15000); // 15 detik timeout
     const start = Date.now();
 
     const response = await fetch(url, {signal: controller.signal});
